26f463055064604030278041147bd3e616920c3d
Pamphile Tagu <pamphile-vianney.tagu@soprasteria.com>
2018-04-26 16:10:09 +0200

    risolved exercice 11 #Security

--- custom/bookstore/bookstorecore/src/my/bookstore/core/setup/CoreSystemSetup.java
+++ custom/bookstore/bookstorecore/src/my/bookstore/core/setup/CoreSystemSetup.java
@@ -1,13 +1,15 @@
 /*
  * [y] hybris Platform
  *
- * Copyright (c) 2000-2016 SAP SE or an SAP affiliate company.
+ * Copyright (c) 2000-2014 hybris AG
  * All rights reserved.
  *
- * This software is the confidential and proprietary information of SAP
+ * This software is the confidential and proprietary information of hybris
  * ("Confidential Information"). You shall not disclose such Confidential
  * Information and shall use it only in accordance with the terms of the
- * license agreement you entered into with SAP.
+ * license agreement you entered into with hybris.
+ *
+ *
  */
 package my.bookstore.core.setup;
 
@@ -19,26 +21,28 @@ import de.hybris.platform.core.initialization.SystemSetup.Type;
 import de.hybris.platform.core.initialization.SystemSetupContext;
 import de.hybris.platform.core.initialization.SystemSetupParameter;
 import de.hybris.platform.core.initialization.SystemSetupParameterMethod;
-import my.bookstore.core.constants.BookstoreCoreConstants;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import my.bookstore.core.constants.BookstoreCoreConstants;
+
 
 /**
  * This class provides hooks into the system's initialization and update processes.
- * 
+ *
  * @see "https://wiki.hybris.com/display/release4/Hooks+for+Initialization+and+Update+Process"
  */
 @SystemSetup(extension = BookstoreCoreConstants.EXTENSIONNAME)
 public class CoreSystemSetup extends AbstractSystemSetup
 {
 	public static final String IMPORT_ACCESS_RIGHTS = "accessRights";
+	public static final String IMPORT_VERIFICATION_SCRIPTS = "verificationScripts";
 
 	/**
 	 * This method will be called by system creator during initialization and system update. Be sure that this method can
 	 * be called repeatedly.
-	 * 
+	 *
 	 * @param context
 	 *           the context provides the selected parameters and values
 	 */
@@ -51,6 +55,8 @@ public class CoreSystemSetup extends AbstractSystemSetup
 
 		importImpexFile(context, "/bookstorecore/import/common/themes.impex");
 		importImpexFile(context, "/bookstorecore/import/common/user-groups.impex");
+
+
 	}
 
 	/**
@@ -60,19 +66,13 @@ public class CoreSystemSetup extends AbstractSystemSetup
 	@SystemSetupParameterMethod
 	public List<SystemSetupParameter> getInitializationOptions()
 	{
-		final List<SystemSetupParameter> params = new ArrayList<>();
+		final List<SystemSetupParameter> params = new ArrayList<SystemSetupParameter>();
 
 		params.add(createBooleanSystemSetupParameter(IMPORT_ACCESS_RIGHTS, "Import Users & Groups", true));
 
 		return params;
 	}
 
-	/**
-	 * This method will be called during the system initialization.
-	 * 
-	 * @param context
-	 *           the context provides the selected parameters and values
-	 */
 	@SystemSetup(type = Type.PROJECT, process = Process.ALL)
 	public void createProjectData(final SystemSetupContext context)
 	{
@@ -80,37 +80,55 @@ public class CoreSystemSetup extends AbstractSystemSetup
 
 		final List<String> extensionNames = getExtensionNames();
 
-		processCockpit(context, importAccessRights, extensionNames, "cmscockpit",
-				"/bookstorecore/import/cockpits/cmscockpit/cmscockpit-users.impex",
-				"/bookstorecore/import/cockpits/cmscockpit/cmscockpit-access-rights.impex");
 
-		processCockpit(context, importAccessRights, extensionNames, "productcockpit",
-				"/bookstorecore/import/cockpits/productcockpit/productcockpit-users.impex",
-				"/bookstorecore/import/cockpits/productcockpit/productcockpit-access-rights.impex",
-				"/bookstorecore/import/cockpits/productcockpit/productcockpit-constraints.impex");
+		//Process.INIT means: applies to System Initialization operation,
+		//Process.UPDATE means: applies to UpdateSystem operation with 'Update Running System' parameter = true
+		//Process.ALL means: applies to all System Initialization / System Update operations
+		//                   (regardless of value of 'Update Running System parameter')
+		if (context.getProcess() == Process.INIT || getBooleanSystemSetupParameter(context, IMPORT_VERIFICATION_SCRIPTS))
+		{
+			importImpexFile(context, "/bookstorecore/import/common/verifyExercise/verifyExercises.impex");
+		}
 
-		processCockpit(context, importAccessRights, extensionNames, "cscockpit",
-				"/bookstorecore/import/cockpits/cscockpit/cscockpit-users.impex",
-				"/bookstorecore/import/cockpits/cscockpit/cscockpit-access-rights.impex");
+		if (importAccessRights && extensionNames.contains("cmscockpit"))
+		{
+			importImpexFile(context, "/bookstorecore/import/cockpits/cmscockpit/cmscockpit-users.impex");
+			importImpexFile(context, "/bookstorecore/import/cockpits/cmscockpit/cmscockpit-access-rights.impex");
+		}
 
-		processCockpit(context, importAccessRights, extensionNames, "reportcockpit",
-				"/bookstorecore/import/cockpits/reportcockpit/reportcockpit-users.impex",
-				"/bookstorecore/import/cockpits/reportcockpit/reportcockpit-access-rights.impex");
+		if (importAccessRights && extensionNames.contains("btgcockpit"))
+		{
+			importImpexFile(context, "/bookstorecore/import/cockpits/cmscockpit/btgcockpit-users.impex");
+			importImpexFile(context, "/bookstorecore/import/cockpits/cmscockpit/btgcockpit-access-rights.impex");
+		}
+
+		if (importAccessRights && extensionNames.contains("productcockpit"))
+		{
+			importImpexFile(context, "/bookstorecore/import/cockpits/productcockpit/productcockpit-users.impex");
+			importImpexFile(context, "/bookstorecore/import/cockpits/productcockpit/productcockpit-access-rights.impex");
+			importImpexFile(context, "/bookstorecore/import/cockpits/productcockpit/productcockpit-constraints.impex");
+		}
+
+		if (importAccessRights && extensionNames.contains("cscockpit"))
+		{
+			importImpexFile(context, "/bookstorecore/import/cockpits/cscockpit/cscockpit-users.impex");
+			importImpexFile(context, "/bookstorecore/import/cockpits/cscockpit/cscockpit-access-rights.impex");
+		}
+
+		if (importAccessRights && extensionNames.contains("reportcockpit"))
+		{
+			importImpexFile(context, "/bookstorecore/import/cockpits/reportcockpit/reportcockpit-users.impex");
+			importImpexFile(context, "/bookstorecore/import/cockpits/reportcockpit/reportcockpit-access-rights.impex");
+		}
 
 		if (extensionNames.contains("mcc"))
 		{
 			importImpexFile(context, "/bookstorecore/import/common/mcc-sites-links.impex");
 		}
+
+		importImpexFile(context, "/bookstorecore/import/common/projectdataSecurity.impex");
 	}
 
-	protected void processCockpit(final SystemSetupContext context, final boolean importAccessRights,
-								final List<String> extensionNames, final String cockpit, final String... files) {
-		if (importAccessRights && extensionNames.contains(cockpit)) {
-			for (String file : files) {
-				importImpexFile(context, file);
-			}
-		}
-	}
 
 	protected List<String> getExtensionNames()
 	{
